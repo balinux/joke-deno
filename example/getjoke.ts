be95be946c5fa90f44cc8ee59daaaadefec477f8
balinux <Ryojuniyantara@gmail.com>
2020-07-09 09:48:58 +0800

    update:  joke example

--- example/getjoke.ts
+++ example/getjoke.ts
@@ -1,4 +1,6 @@
-import { Joke } from '../mod.ts'
+import { Joke, Car } from '../mod.ts'
 const request = new Joke()
+const newCar = new Car("Lambo")
 const dataReq = await request.getJoke();
 console.log(dataReq);
+console.log(newCar);
